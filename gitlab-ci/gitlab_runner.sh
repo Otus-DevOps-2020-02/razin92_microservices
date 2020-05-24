c4d9ff371339ea675aaa7fb45161108f991be370
Vyacheslav Razin <s.razin92@gmail.com>
2020-05-24 18:00:49 +0500

    docker exec and docker image for docker runner

--- gitlab-ci/gitlab_runner.sh
+++ gitlab-ci/gitlab_runner.sh
@@ -4,4 +4,5 @@
 docker run -d --name gitlab-runner --restart always \
 -v /srv/gitlab-runner/config:/etc/gitlab-runner \
 -v /var/run/docker.sock:/var/run/docker.sock \
+-v /usr/bin/docker:/usr/bin/docker \
 gitlab/gitlab-runner:latest
--- gitlab-ci/runner_docker_register.sh
+++ gitlab-ci/runner_docker_register.sh
@@ -7,7 +7,7 @@ docker exec -it gitlab-runner gitlab-runner register \
  --locked=false \
  --url=http://$(docker-machine ip gitlab-ci)/ \
  --executor=docker \
- --docker-image=alpine:latest \
+ --docker-image=ruby:2.4.2 \
  --tag-list="linux,xenial,ubuntu,docker" \
  --description="docker-runner" \
  --registration-token $REGISTRATION_TOKEN
